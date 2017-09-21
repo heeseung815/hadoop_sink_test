afa10632777076017f6bbd72840995ecd6da3b90
HeeSeung, Cho <hscho@uangel.com>
2017-09-21 19:43:44 +0900

    no message

--- src/main/scala/HdfsPublisher.scala
+++ src/main/scala/HdfsPublisher.scala
@@ -7,8 +7,10 @@ import akka.stream.{Attributes, Inlet, SinkShape}
 import com.typesafe.config.ConfigFactory
 import common.TimeUtil
 import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.permission.FsAction
 import org.apache.hadoop.fs.{FSDataOutputStream, FileStatus, FileSystem, Path}
 
+// TODO: 리모트로 하둡 파일시스템 연결이 안되는 경우 재시도하는 로직을 생각해 두어야한다.
 object HdfsPublisher extends GraphStage[SinkShape[String]] {
 
   // for sink
@@ -20,12 +22,14 @@ object HdfsPublisher extends GraphStage[SinkShape[String]] {
   val limit = config.getInt("hadoop.fs.file.limit")
   val basePath = config.getString("hadoop.fs.file.path")
   val fileExt = config.getString("hadoop.fs.file.extension")
+  val fileOption = config.getString("hadoop.fs.file.option")
 
   // for hadoop configuration
   val conf = new Configuration()
   conf.set("fs.default.name", "hdfs://localhost:9000")
+  conf.set("dfs.support.append", "true")
   System.setProperty("HADOOP_USER_NAME", "hscho")
-  val fs: FileSystem = FileSystem.get(conf)
+  var fs: FileSystem = FileSystem.get(conf)
   private var os: FSDataOutputStream = _
 
   // for sensor
@@ -43,28 +47,85 @@ object HdfsPublisher extends GraphStage[SinkShape[String]] {
 
       // This requests one element at the Sink startup
       override def preStart(): Unit = {
-        println("HdfsPublisher preStart called...")
+        println("# preStart called...\n")
         // 입력 값 최초 요청 시에 설정한 이름으로 파일을 생성하고 해당 파일에 쓰기 위한 스트림을 오픈한다.
-        currentFilePath = new Path(basePath + "/" + TimeUtil.getCurrentYear + "/" + TimeUtil.getCurrentMonth + "/" + TimeUtil.getCurrentDay + "/" + sensorId + "_" + TimeUtil.getCurrentTime + fileExt)
-        println(s"# Current file path: ${currentFilePath.getName}")
-        os = fs.create(currentFilePath)
+//        currentFilePath = new Path(basePath + "/" + TimeUtil.getCurrentYear + "/" + TimeUtil.getCurrentMonth + "/" + TimeUtil.getCurrentDay + "/" + sensorId + "_" + TimeUtil.getCurrentTime + fileExt)
+//        println(s"# Current file path: ${currentFilePath.getName}")
+//        os = fs.create(currentFilePath)
         pull(in)
       }
 
       setHandler(in, new InHandler {
         override def onPush(): Unit = {
-          // 쓰고자 하는 파일의 사이즈가 기준치를 넘어섰는지 먼저 체크한다.
-          val currentFileStatus = fs.getFileStatus(currentFilePath)
-          if (currentFileStatus.getLen >= (limit * 1024)) {
-            //
+          if (currentFilePath == null) currentFilePath = new Path(basePath + "/" + TimeUtil.getCurrentYear + "/" + TimeUtil.getCurrentMonth + "/" + TimeUtil.getCurrentDay + "/" + sensorId + "_" + TimeUtil.getCurrentTime + fileExt)
+          // 최초 파일 또는 새로운 파일
+          if (fs.exists(currentFilePath)) {
+            println("exist")
+            val fileStatus = fs.getFileStatus(currentFilePath)
+            if (fileStatus.getLen >= (limit * 1024)) {
+              println("exist but exceed limit")
+              TimeUtil.resetDate
+              currentFilePath = new Path(basePath + "/" + TimeUtil.getCurrentYear + "/" + TimeUtil.getCurrentMonth + "/" + TimeUtil.getCurrentDay + "/" + sensorId + "_" + TimeUtil.getCurrentTime + fileExt)
+              os = fs.create(currentFilePath)
+            }
+            os = fs.append(currentFilePath)
+//            os = fs.create(currentFilePath)
+          } else {
+            println("not exist")
+            os = fs.create(currentFilePath)
           }
 
-          os.close()
-          os = null
+          // TODO: 플로우로부터 받은 데이터 유효성 검사 로직 필요한지 여부 확인 할 것
+          val data: String = grab(in)
+          println(s"# os: $os")
+
+//          PrintWriter()
+          os.write(data.getBytes())
+          print(s"# Data : $data")
+
+//          os.close()
+          if (os != null) {
+            println("stream close")
+            os.close()
+            os = null
+            println("null set")
+          }
+
+          val read = scala.io.Source.fromInputStream(fs.open(currentFilePath))
+          read.getLines().foreach(println)
+
+
+          println("pull(in)\n")
+          pull(in)
+/*
+          // 쓰고자 하는 파일의 사이즈가 기준치를 넘어섰는지 먼저 체크한다.
+          val currentFileStatus = fs.getFileStatus(currentFilePath)
+          if (currentFileStatus.getLen == 0) {
+            println("# create called ")
+            os.write(data.getBytes())
+            os.close()
+            os = null
+          } else if (currentFileStatus.getLen >= (limit * 1024)) {
+            println("# create called due to size limit")
+            // 넘어선 경우 새로운 파일을 만들고 스트림을 생성한다.
+            TimeUtil.resetDate
+            currentFilePath = new Path(basePath + "/" + TimeUtil.getCurrentYear + "/" + TimeUtil.getCurrentMonth + "/" + TimeUtil.getCurrentDay + "/" + sensorId + "_" + TimeUtil.getCurrentTime + fileExt)
+            os = fs.create(currentFilePath)
+          } else {
+            println("# append called")
+            println(s"##### $os")
+            os = fs.append(currentFilePath)
+          }
+
+          // append
+          // create
+
+
+
 
           pull(in)
 
-
+*/
           /*
           if (os == null) {
             println(s"os closed!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
